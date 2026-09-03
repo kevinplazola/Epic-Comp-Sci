33a4a3da6a3f549498013e4d4d9c30a459728d0b
kevinplazola <kevinplazolalugo391@gmail.com>
2026-09-03 09:42:28 -0600

    Delete deep_thought..py

--- deep_thought..py
+++
@@ -1,5 +0,0 @@
-answer = input("What is the answer to life, the universe, and everything? ").lower()
-if answer in ["42", "forty-two", "forty two"]:
-    print("Correct!")
-else:
-    print("Incorrect.")
