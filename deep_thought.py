d1990000ffd6e66acd5fb9e5f02b9ea6ff2f010f
kevinplazola <kevinplazolalugo391@gmail.com>
2026-09-03 09:46:00 -0600

    Add files via upload

---
+++ deep_thought.py
@@ -0,0 +1,5 @@
+answer = input("What is the answer to life, the universe, and everything? ").lower()
+if answer in ["42", "forty-two", "forty two", "fourty-two", "fourty two"]:
+	print("Correct!")
+else:
+	print("Incorrect!")
